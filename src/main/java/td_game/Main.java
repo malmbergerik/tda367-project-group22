ee098a313c77ddb93cdc6bf219a25eccbe316ff2
Axel Olsson <axel.n.olsson@outlook.com>
2025-11-13 10:52:15 +0100

    Test window frame

--- src/main/java/td_game/Main.java
+++ src/main/java/td_game/Main.java
@@ -1,22 +1,10 @@
 package td_game;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import td_game.view.WindowFrame;
 
 public class Main {
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater (() -> {
-            JFrame window = new JFrame();
-
-            window.setTitle("Tower defence game");
-
-            window.setSize(1200, 800);
-
-            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-            window.setLocationRelativeTo(null);
-
-            window.setVisible(true);
-        });
+        WindowFrame frame = new WindowFrame();
     }
 }
