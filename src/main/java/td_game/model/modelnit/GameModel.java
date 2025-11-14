2a947e051fff30c2ca3ec95d9355c4c0f7369ba5
Axel <axel.n.olsson@outlook.com>
2025-11-14 10:18:46 +0100

    Created a baseclass for the Model

--- src/main/java/td_game/model/modelnit/GameModel.java
+++ src/main/java/td_game/model/modelnit/GameModel.java
@@ -1,11 +1,52 @@
 package td_game.model.modelnit;
 
-protected abstract class1 AModelInitializer {
+import td_game.model.GameState;
+
+import java.util.ArrayList;
+
+public class GameModel implements GameObservable {
+    private List<GameObservers> observers = new ArrayList<>();
+    private GameState currentState = GameState.Menu;
+
     /*
-    Map
-    enemy list
-    enemy spawner
-    tower list
+    Här vill vi ha listor för torn, enemies, pengar, spelaren...
+
+    private List<Enemy> activeEnemies = new ArrayList<>();
+    private List<Tower> placedTowers = new ArrayList<>();
+    private List<Projectiles> activeProjectiles = new ArrayList<>();
 
      */
+
+    public void setGameState(GameState gameState) {
+        // TODO
+    }
+
+    public void gameTick() {
+        if (currentState == GameState.PLAYING) {
+            // TODO
+
+           /*
+            Här vill vi ha alla tower.update(), enemies.update()...
+
+            */
+            notifyObserver();
+        }
+    }
+
+    @Override
+    public void registerObserver(GameObserver observer) {
+
+    }
+
+    @Override
+    public void unregisterObserver(GameObserver observer) {
+
+    }
+
+    @Override
+    public void notifyObserver() {
+        for (GameObservable observer: observers) {
+            observer.update();
+        }
+    }
 }
