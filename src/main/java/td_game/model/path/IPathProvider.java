2b1b6746bfeff469e557125752385d34b9f4e106
Abdullah <shamal@chalmers.com>
2025-11-18 19:01:00 +0100

    Interface IPathProvider created

--- src/main/java/td_game/model/path/IPathProvider.java
+++ src/main/java/td_game/model/path/IPathProvider.java
@@ -1,4 +1,20 @@
 package td_game.model.path;
+import td_game.model.map.IMap;
+
+/**
+ * Simple provider interface for getting a Path for a given map.
+ * Consumers (EnemyFactory, enemies, controllers) should request a Path
+ * from an implementation (eg PathManager).
+ */
 
 public interface IPathProvider {
+    /**
+     * Return the cached/singleton Path for the provided map instance.
+     * Implementations may compute it lazily or require explicit precomputation.
+     *
+     * @param map the map to get the path for
+     * @return Path for the map (non-null)
+     * @throws IllegalArgumentException if no path can be found
+     */
+    Path getPathForMap(IMap map);
 }
