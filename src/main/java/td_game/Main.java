3e80236da53a47a1ce94469439b8e7f6b72ca252
Axel <axel.n.olsson@outlook.com>
2025-11-20 19:02:49 +0100

    Small change to make it work

--- src/main/java/td_game/Main.java
+++ src/main/java/td_game/Main.java
@@ -1,19 +1,15 @@
 package td_game;
 
-import td_game.controller.ViewController;
 import td_game.model.map.*;
 import td_game.model.modelnit.GameModel;
 import td_game.view.WindowFrame;
 
-import javax.swing.text.View;
-
 public class Main {
 
     public static void main(String[] args) {
-        GameModel gameModel = new GameModel(576,640,16);
-        WindowFrame frame = new WindowFrame(1024, 768);
-        frame.displayMenuView();
-        ViewController gameController = new ViewController(frame);
+        GameModel gameModel = new GameModel(720,576,16);
+        WindowFrame frame = new WindowFrame(1024, 768, gameModel);
+        frame.showView("MENU_VIEW");
     }
 }
 
