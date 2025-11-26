9d9e3a1086e335a9dfe1d85d5c562e6b86908f50
malmbergerik <erikmalmberg45@gmail.com>
2025-11-26 15:06:25 +0100

    Fixed loading map added test

--- src/test/java/td_game/model/map/MapLoaderTest.java
+++ src/test/java/td_game/model/map/MapLoaderTest.java
@@ -18,4 +18,17 @@ class MapLoaderTest {
         assertEquals(pathTile.getType(), tile2.getType());
 
     }
+
+    @Test
+    void loadMapStartEnd() {
+        GridMap gridMap = MapLoader.loadMap("levels/lvl1.txt",16);
+        PathTile tile1 = (PathTile) gridMap.getTile(7,0);
+        PathTile pathStart = new PathTile(PathType.START);
+        assertEquals(pathStart.getPathType(), tile1.getPathType());
+
+        PathTile tile2 = (PathTile) gridMap.getTile(2,14);
+        PathTile pathEnd = new PathTile(PathType.END);
+        assertEquals(pathEnd.getPathType(), tile2.getPathType());
+
+    }
 }
