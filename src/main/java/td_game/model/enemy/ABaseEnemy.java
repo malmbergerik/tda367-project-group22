54d4e0e5cbe6047537d13c6966e0b84e93a533fc
masten11 <mats.brategren@gmail.com>
2025-12-02 11:05:17 +0100

    moved move function to Base class

--- src/main/java/td_game/model/enemy/ABaseEnemy.java
+++ src/main/java/td_game/model/enemy/ABaseEnemy.java
@@ -2,6 +2,7 @@ package td_game.model.enemy;
 
 import td_game.model.path.Path;
 import td_game.model.path.Waypoint;
+import java.lang.Math;
 
 public abstract class ABaseEnemy implements ITargetable, IMoveable, IDamageable {
     protected int health;
@@ -69,5 +70,39 @@ public abstract class ABaseEnemy implements ITargetable, IMoveable, IDamageable 
     public int getHealth() {return  health;}
 
     @Override
-    public abstract void move();
+    public  void move() {
+        if (hasReachedEnd()) {
+            return; // Stop if the end of the path is reached.
+        }
+
+        double speedForTick = speed; // The total distance the enemy can move this tick.
+
+        // Loop as long as the enemy has movement speed left for this tick and has not reached the end.
+        while (speedForTick > 0 && !hasReachedEnd()) {
+            Waypoint targetWaypoint = path.get(currentWaypointIndex);
+            double targetX = targetWaypoint.getX();
+            double targetY = targetWaypoint.getY();
+
+            double dx = targetX - posX;
+            double dy = targetY - posY;
+
+            double distanceToWaypoint = Math.sqrt(dx * dx + dy * dy);
+
+            if (distanceToWaypoint <= speedForTick) {
+                // The enemy can reach the waypoint in this step.
+                posX = targetX;
+                posY = targetY;
+                currentWaypointIndex++;
+                speedForTick -= distanceToWaypoint; // Subtract the distance traveled from the speed budget.
+            }
+            else {
+                // The enemy cannot reach the waypoint in this step, so move towards it.
+                double moveX = (dx / distanceToWaypoint) * speedForTick;
+                double moveY = (dy / distanceToWaypoint) * speedForTick;
+                posX += moveX;
+                posY += moveY;
+                speedForTick = 0; // All speed for this tick has been used.
+            }
+        }
+    };
 }
--- src/main/java/td_game/model/enemy/Skeleton.java
+++ src/main/java/td_game/model/enemy/Skeleton.java
@@ -1,6 +1,5 @@
 package td_game.model.enemy;
 
-import java.lang.Math;
 import td_game.model.path.Path;
 import td_game.model.path.Waypoint;
 
@@ -16,44 +15,6 @@ public class Skeleton extends ABaseEnemy{
     @Override
     public void takeDamage(int amount) {
         health -= amount;
-    }
-
-
-    @Override
-    public void move() {
-        if (hasReachedEnd()) {
-            return; // Stop if the end of the path is reached.
-        }
-
-        double speedForTick = speed; // The total distance the enemy can move this tick.
-
-        // Loop as long as the enemy has movement speed left for this tick and has not reached the end.
-        while (speedForTick > 0 && !hasReachedEnd()) {
-            Waypoint targetWaypoint = path.get(currentWaypointIndex);
-            double targetX = targetWaypoint.getX();
-            double targetY = targetWaypoint.getY();
-
-            double dx = targetX - posX;
-            double dy = targetY - posY;
-
-            double distanceToWaypoint = Math.sqrt(dx * dx + dy * dy);
-
-            if (distanceToWaypoint <= speedForTick) {
-                // The enemy can reach the waypoint in this step.
-                posX = targetX;
-                posY = targetY;
-                currentWaypointIndex++;
-                speedForTick -= distanceToWaypoint; // Subtract the distance traveled from the speed budget.
-            }
-            else {
-                // The enemy cannot reach the waypoint in this step, so move towards it.
-                double moveX = (dx / distanceToWaypoint) * speedForTick;
-                double moveY = (dy / distanceToWaypoint) * speedForTick;
-                posX += moveX;
-                posY += moveY;
-                speedForTick = 0; // All speed for this tick has been used.
-            }
-        }
     }
 
 }
