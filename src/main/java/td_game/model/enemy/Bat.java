0c8a846768aea504d7e5ee90745ea12b482edffb
Abdullah <shamal@chalmers.com>
2025-12-06 15:45:45 +0100

    Bat png

--- src/main/java/td_game/model/enemy/Bat.java
+++ src/main/java/td_game/model/enemy/Bat.java
@@ -4,8 +4,8 @@ import td_game.model.path.Path;
 
 public class Bat extends PathFollowingEnemy {
 
-    private static final int ENEMY_WIDTH = 6;
-    private static final int ENEMY_HEIGHT = 6;
+    private static final int ENEMY_WIDTH = 16;
+    private static final int ENEMY_HEIGHT = 16;
     private static final String NAME = "Bat"; //
 
     public Bat(int health, double speed, Path path){
