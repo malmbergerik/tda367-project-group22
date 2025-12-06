a5a527e68bb0a46f600cb10dcd4fceca955f6d80
masten11 <mats.brategren@gmail.com>
2025-12-06 14:07:17 +0100

    Created class that represents collection of groups

--- src/main/java/td_game/model/waves/Wave.java
+++ src/main/java/td_game/model/waves/Wave.java
@@ -1,4 +1,26 @@
 package td_game.model.waves;
 
-public class EnemySpawnConfig {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Wave {
+    private final int waveNumber;
+    private final List<WaveGroup> waveGroups;
+
+    public Wave(int waveNumber) {
+        this.waveNumber = waveNumber;
+        this.waveGroups = new ArrayList<>();
+    }
+
+    public void addWaveGroup(WaveGroup group) {
+        waveGroups.add(group);
+    }
+
+    public List<WaveGroup> getWaveGroups() {
+        return waveGroups;
+    }
+
+    public int getWaveNumber() {
+        return waveNumber;
+    }
 }
