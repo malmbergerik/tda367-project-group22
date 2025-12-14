9e3abeb66fd783f792b6473e2a5b45c7aaec35e4
Axel <axel.n.olsson@outlook.com>
2025-12-14 15:46:48 +0100

    Added javadoc comments

--- src/main/java/td_game/view/listener/IMenuView.java
+++ src/main/java/td_game/view/listener/IMenuView.java
@@ -1,5 +1,8 @@
 package td_game.view.listener;
 
+/**
+ * An interface for listening to menu view events.
+ */
 public interface IMenuView {
 
     void addPlayListener(java.awt.event.ActionListener listener);
--- src/main/java/td_game/view/listener/ITowerActionListener.java
+++ src/main/java/td_game/view/listener/ITowerActionListener.java
@@ -1,5 +1,8 @@
 package td_game.view.listener;
 
+/**
+ * An interface for listening to tower actions such as selling.
+ */
 public interface ITowerActionListener {
     void onTowerSell();
 }
