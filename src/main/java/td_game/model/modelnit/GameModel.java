fbda86a803e3382f53b5cd84107cbefe8ef55628
masten11 <mats.brategren@gmail.com>
2025-12-22 15:57:07 -0500

    Changes

--- src/main/java/td_game/model/modelnit/GameModel.java
+++ src/main/java/td_game/model/modelnit/GameModel.java
@@ -99,11 +99,11 @@ public class GameModel implements GameObservable, IUpdatable, IPlayerObserver {
         this.moneySystem = new MoneySystem(player);
 
         this.enemyFactory = new EnemyFactory();
-        enemyFactory.registerFactory("Slime",    path -> new Slime(10, 0.2, path,1));
-        enemyFactory.registerFactory("Skeleton", path -> new Skeleton(100, 0.3, path,3));
-        enemyFactory.registerFactory("Golem",    path -> new Golem(10000, 0.15, path,100));
-        enemyFactory.registerFactory("Bat",      path -> new Bat(40, 0.6, path,1));
-        enemyFactory.registerFactory("BabyOrc",  path -> new BabyOrc(200, 0.8, path,4));
+        enemyFactory.registerFactory("Slime",    path -> new Slime(40, 0.2, path,1));
+        enemyFactory.registerFactory("Skeleton", path -> new Skeleton(100, 0.4, path,3));
+        enemyFactory.registerFactory("Golem",    path -> new Golem(10000, 0.1, path,100));
+        enemyFactory.registerFactory("Bat",      path -> new Bat(50, 0.7, path,2));
+        enemyFactory.registerFactory("BabyOrc",  path -> new BabyOrc(250, 0.6, path,4));
 
         this.enemyManager = new EnemyManager(this.activeEnemies, this, damageSystem, moneySystem);
 
