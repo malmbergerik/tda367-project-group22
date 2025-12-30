3d864ff6542c48d06819768ad7db2f075caec5cd
masten11 <mats.brategren@gmail.com>
2025-12-30 11:59:27 -0500

    CHanges

--- src/main/java/td_game/model/towers/CanonTower.java
+++ src/main/java/td_game/model/towers/CanonTower.java
@@ -38,7 +38,7 @@ public class CanonTower extends ATower{
     {
         super(x,y,25,
                 new SingleAttackStrategy(
-                        new ProjectileFactory(new BasicMovementStrategy(1),new BasicPierceStrategy(5),new BasicDamageTypeStrategy(1),new BasicLifeTimeStrategy(120),new BasicRoundSizeStrategy(50,50), "Bullet"),
+                        new ProjectileFactory(new BasicMovementStrategy(1),new BasicPierceStrategy(5),new BasicDamageTypeStrategy(5),new BasicLifeTimeStrategy(120),new BasicRoundSizeStrategy(50,50), "Bullet"),
                         projectileManager
                 ),
                 new NormalCircularRangeStrategy(80),
